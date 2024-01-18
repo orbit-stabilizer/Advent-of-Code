1d990a68ccd8963d871f14fcf70ff9b6100897f5
orbit-stabilizer <deowra.kamal@gmail.com>
2024-01-17 23:24:07 -0800

    Update part two to use part one

--- 2023/2/PartTwo.hs
+++ 2023/2/PartTwo.hs
@@ -11,30 +11,8 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T.IO
 import qualified Data.Text.Read as T.R 
 
--- * Globals * -- 
+import PartOne (parse, makeGame, Game (..))
 
-maxRedCubes = 12
-maxGreenCubes = 13
-maxBlueCubes = 14
-
--- * Data Definitions * --
-
-data Game = Game
-  { gameId     :: Int
-  , redCubes   :: Int
-  , greenCubes :: Int
-  , blueCubes  :: Int
-  }
-
--- * Operator Definitions * --
-
-(|>) :: a -> (a -> b) -> b
-(|>) = (&)
-
-(!=) :: Eq a => a -> a -> Bool
-(!=) = (/=)
-
--- * Code * --
 
 main :: IO ()
 main = do
@@ -42,31 +20,6 @@ main = do
   let games = map makeGame input
   print $ calculate games
 
-parse :: T.Text -> [T.Text]
-parse text =
-  text
-    |> T.filter (!= ' ')
-    |> T.replace ";" ","
-    |> T.lines
-    |> map (T.dropWhile (not . isDigit))
-
-makeGame :: T.Text -> Game
-makeGame text = Game gameId redCubes greenCubes blueCubes
-  where
-    gameId     = either (error "gameId parse error") fst . T.R.decimal $ text
-    redCubes   = getMaxColour "red"  
-    greenCubes = getMaxColour "green"
-    blueCubes  = getMaxColour "blue"
-
-    pText      = T.split (== ',') . T.takeWhileEnd (!= ':') $ text
-
-    getMaxColour :: T.Text -> Int
-    getMaxColour colour =
-      pText
-        |> filter (T.isInfixOf colour)
-        |> map  (either (error "colour parse error") fst . T.R.decimal)
-        |> maximum
-    
 calculate :: [Game] -> Int
 calculate = sum . map power
   where
